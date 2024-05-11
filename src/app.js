500b8e6c2f0f220c3f469688dbbee0bd4a4e5bfb
Matheus-santos7 <matheussantosgc@icloud.com>
2024-05-10 21:54:23 -0300

    Fix: correçao de bugs

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
+// Importa o framework Express, que será utilizado para criar o servidor web
 const express = require('express');
 
+// Importa as rotas principais definidas no arquivo 'routes.js'
+const routes = require('./routes');
+
+// Cria uma instância do aplicativo Express
 const app = express();
 
-app.use(express.json());
+// Configura as rotas do aplicativo Express, passando a instância do aplicativo como parâmetro
+routes(app);
 
-app.get('/teste', (req, res) => {
-  res
-    .status(200)
-    .send({ mensagem: 'boas-vindas à API' });
-});
-
+// Exporta o aplicativo Express para que ele possa ser iniciado em outro arquivo
 module.exports = app;
