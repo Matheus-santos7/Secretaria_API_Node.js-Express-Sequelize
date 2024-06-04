6162e7cce6cd4b17d5c38c3f387bc67b5e7faa23
Matheus-santos7 <matheussantosgc@icloud.com>
2024-06-03 23:39:25 -0300

    style: coments in code app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
+// Importa o módulo 'express', que é um framework web para Node.js.
+// 'express' facilita a criação de servidores e o gerenciamento de rotas.
 const express = require('express');
+
+// Importa as rotas definidas no arquivo './routes'.
+// Isso permite separar a lógica das rotas do restante do código do servidor.
 const routes = require('./routes');
 
+// Cria uma instância do aplicativo Express.
+// 'app' será usado para definir rotas e outras configurações do servidor.
 const app = express();
+
+// Chama a função 'routes' passando a instância 'app'.
+// Isso configura todas as rotas que foram definidas no módulo './routes'.
 routes(app);
 
+// Exporta a instância do aplicativo Express.
+// Isso permite que outros arquivos utilizem essa instância, como no arquivo principal do servidor que vimos anteriormente.
 module.exports = app;
