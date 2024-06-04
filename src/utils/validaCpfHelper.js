3ed9e2fec438594bfbbe2602c31e19f9cb6b0dde
Matheus-santos7 <matheussantosgc@icloud.com>
2024-06-03 23:42:20 -0300

    style: coments in code validaCpfHelper.js

--- src/utils/validaCpfHelper.js
+++ src/utils/validaCpfHelper.js
@@ -1,4 +1,10 @@
+// Exporta uma função anônima que recebe um parâmetro 'cpf'.
+// 'cpf' é esperado ser uma string representando um número de CPF (Cadastro de Pessoas Físicas, usado no Brasil).
 module.exports = (cpf) => {
+  // Verifica se o comprimento de 'cpf' é diferente de 11.
+  // Um CPF válido tem exatamente 11 dígitos. Se não tiver, a função retorna 'false'.
   if (cpf.length !== 11) return false;
+  
+  // Se o comprimento for 11, a função retorna 'true'.
   return true;
 };
