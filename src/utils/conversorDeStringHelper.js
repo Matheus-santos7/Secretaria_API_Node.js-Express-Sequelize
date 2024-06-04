2ac90e9b8617fa27f6f4e5b2ea473433b7528e07
Matheus-santos7 <matheussantosgc@icloud.com>
2024-06-03 23:43:36 -0300

    style: coments in code conversorDeStrings

--- src/utils/conversorDeStringHelper.js
+++ src/utils/conversorDeStringHelper.js
@@ -1,8 +1,15 @@
+// Exporta uma função anônima que recebe um parâmetro 'objetoParams'.
+// 'objetoParams' é esperado ser um objeto cujas propriedades podem conter valores que precisam ser convertidos para números.
 module.exports = (objetoParams) => {
+  // Utiliza um loop 'for...in' para iterar sobre todas as propriedades do objeto 'objetoParams'.
   for (let propriedade in objetoParams) {
+    // Verifica se o nome da propriedade contém 'Id' ou 'id' usando uma expressão regular.
+    // A expressão regular /Id|id/ testa se a string da propriedade contém 'Id' ou 'id' em qualquer lugar.
     if (/Id|id/.test(propriedade)) {
+      // Converte o valor da propriedade para um número usando a função 'Number' e o atribui de volta à propriedade.
       objetoParams[propriedade] = Number(objetoParams[propriedade]);
     }
   }
+  // Retorna o objeto modificado.
   return objetoParams;
 };
